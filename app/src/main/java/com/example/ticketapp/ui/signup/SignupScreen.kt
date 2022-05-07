8b3d1e712618229cdf23d5f88f6210413a88f2a5
TimmyNguyen <tnguyen982@horizon.csueastbay.edu>
2022-05-07 13:40:14 -0700

    Fixed ui with signup and login

--- app/src/main/java/com/example/ticketapp/ui/signup/SignupScreen.kt
+++ app/src/main/java/com/example/ticketapp/ui/signup/SignupScreen.kt
@@ -2,27 +2,56 @@
 package com.example.ticketapp.ui.signup
 
 import android.inputmethodservice.Keyboard
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ticketapp.ui.dashboard.DashBoardBody
 import com.example.ticketapp.ui.login.DontHaveAnAccount
 
 @Composable
 fun SignupScreen(
+    onClickSignup: (email: String, pass: String, role: String) -> Unit,
+    onClickBack: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("") },
+                backgroundColor = Color.White,
+                navigationIcon = {
+                    IconButton(onClick = { onClickBack() }) {
+                        Icon(Icons.Filled.ArrowBack, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ){
+        SignupBody(onClickSignup = onClickSignup)
+    }
+}
+
+@Composable
+fun SignupBody(
     onClickSignup: (email: String, pass: String, role: String) -> Unit
 ) {
     var name by rememberSaveable { mutableStateOf("") }
     var email by rememberSaveable { mutableStateOf("") }
     var pass by rememberSaveable { mutableStateOf("") }
-    val selectedRole by remember { mutableStateOf("") }
+    val selectedRole by remember { mutableStateOf("Developer") }
+    Log.i("SignupScreen", "Selected Role: ${selectedRole}")
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -66,23 +95,25 @@ fun SignupPass(pass: String, onTextChange: (String) -> Unit) {
 
 @Composable
 fun ChooseRole(selectedRole: String) {
-    val roleOptions = listOf("Developer", "Program Manager")
+    val radioOptions = listOf("Developer", "Program Manager")
+    val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[0]) }
     // Note that Modifier.selectableGroup() is essential to ensure correct accessibility behavior
-    Column(Modifier.selectableGroup()) {
-        roleOptions.forEach { text ->
+    Column(Modifier.selectableGroup().padding(start = 40.dp, end = 40.dp)) {
+        radioOptions.forEach { text ->
             Row(
                 Modifier
+                    .fillMaxWidth()
                     .height(56.dp)
                     .selectable(
-                        selected = (text == selectedRole),
-                        onClick = {},
+                        selected = (text == selectedOption),
+                        onClick = { onOptionSelected(text) },
                         role = Role.RadioButton
                     )
                     .padding(horizontal = 16.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 RadioButton(
-                    selected = (text == selectedRole),
+                    selected = (text == selectedOption),
                     onClick = null // null recommended for accessibility with screenreaders
                 )
                 Text(
